9ddc4c4c8947e7311548d8fad6c2ad4b4fe34c74
Enrico Blackwell <pumpkintitan11@gmail.com>
2020-09-29 14:26:56 -0600

    React router and a little bit of home page

---
+++ coderzz/src/Views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './Home.css';
+
+function Home() {
+    return (
+        <div>
+            <h1>Yo</h1>
+        </div>
+    );
+}
+
+export default Home;
