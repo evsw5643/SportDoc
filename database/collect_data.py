0d20a2e9a9b8bd93336e5630cf2784ff554fbd62
willemhscott <willem.scott@colorado.edu>
2020-10-08 15:51:07 -0600

    game data

--- database/collect_data.py
+++ database/collect_data.py
@@ -2,11 +2,10 @@
 import time
 from datetime import datetime
 
+import pandas as pd
 import sqlalchemy  # Package for accessing SQL databases via Python
 from sportsreference.mlb.boxscore import Boxscores
 from sportsreference.nba.teams import Teams
-
-import pandas as pd
 
 # Connect to database (Note: The package psychopg2 is required for Postgres to work with SQLAlchemy)
 engine = sqlalchemy.create_engine("postgresql://ubuntu:password@3.17.77.33/sportdoc")
@@ -14,6 +13,7 @@ con = engine.connect()
 
 # Verify that there are no existing tables
 print(engine.table_names())
+timeout = 60
 
 
 def do_year(year):
@@ -25,7 +25,7 @@ def do_year(year):
         frames.to_sql("teams", con, if_exists='append')
     except Exception as e:
         print(e)
-        time.sleep(5 * 60)
+        time.sleep(timeout)
         teams = Teams(year)
         frames = teams.dataframes
         frames['year'] = year
@@ -39,7 +39,7 @@ def do_year(year):
         do_games()
     except Exception as e:
         print(e)
-        time.sleep(5 * 60)
+        time.sleep(timeout)
         do_games()
 
 
@@ -53,16 +53,24 @@ def do_players(team):
             pdf.to_sql("players", con, if_exists='append')
         except Exception as e:
             print(e)
-            time.sleep(5 * 60)
+            time.sleep(timeout)
             pdf = player.dataframe
             pdf['year'] = year
             pdf['team'] = team.name
             pdf['player_name'] = player.name
             pdf.to_sql("players", con, if_exists='append')
 
+
 def do_games():
-    games = Boxscores(datetime(year, 1, 1), datetime(year + 1, 1, 1))
-    pd.DataFrame(games.games).to_sql("games", con, if_exists='append')
+    games = Boxscores(datetime(year, 9, 1), datetime(year + 1, 7, 1))
+    ngames = []
+    for month, games in games.games.items():
+        for game in games:
+            game["month"] = datetime.strptime(month, "%m-%d-%Y")
+            ngames.append(game)
+
+    df = pd.DataFrame(ngames)
+    df.to_sql("games", con, if_exists='append')
 
 
 for year in range(2011, 2021):
@@ -70,7 +78,7 @@ for year in range(2011, 2021):
         do_year(year)
     except Exception as e:
         print(e)
-        time.sleep(60 * 5)
+        time.sleep(timeout)
         do_year(year)
 
 con.close()
--- database/rating.py
+++ database/rating.py
@@ -1,9 +1,32 @@
 # ATTENTION! Do not run this file unless you want to reset the database and re grab all the data!
 import time
 from datetime import datetime
+import pandas as pd
 
-import sqlalchemy  # Package for accessing SQL databases via Python
 from sportsreference.mlb.boxscore import Boxscores
 
-games = Boxscores(datetime(2019, 9, 1), datetime(2020, 6, 1))
-print(games.games)
+year = 2019
+def do_games():
+    games = Boxscores(datetime(year, 9, 1), datetime(year + 1, 7, 1))
+    ngames = []
+    for month, games in games.games.items():
+        for game in games:
+            game["month"] = datetime.strptime(month, "%m-%d-%Y")
+            ngames.append(game)
+
+    df = pd.DataFrame(ngames)
+    print(df.head())
+
+do_games()
+
+
+# import sqlalchemy  # Package for accessing SQL databases via Python
+#
+#
+# engine = sqlalchemy.create_engine("postgresql://ubuntu:password@3.17.77.33/sportdoc")
+# con = engine.connect()
+#
+# sel = sqlalchemy.select()
+# con.execute(sel)
+#
+# con.close()
