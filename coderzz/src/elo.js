b2ed5ea204a156640b48ea21cafa8dca4489c92b
willemhscott <willem.scott@colorado.edu>
2020-11-18 11:04:46 -0700

    aaaaaaaaaaaaaaaa

--- coderzz/src/elo.js
+++ coderzz/src/elo.js
@@ -1,4 +1,9 @@
-Elo = (function () {
+var $ = require("jquery");
+const fetch = require("node-fetch");
+
+
+let Elo = (function () {
+
     function getRatingDelta(myRating, opponentRating, myGameResult) {
         if ([0, 0.5, 1].indexOf(myGameResult) === -1) {
             return null;
@@ -17,11 +22,13 @@ Elo = (function () {
         return 1 / (1 + Math.pow(10, (opponentRating - myRating) / 400))
     }
 
+
     return {
         getRatingDelta: getRatingDelta,
         getNewRating: getNewRating,
-        getWinChance: getWinChance // Elo.getWinChance
+        getWinChance: getWinChance, // Elo.getWinChance,
     };
+
 })();
 
 function get(object, key, default_value) {
@@ -32,27 +39,36 @@ function get(object, key, default_value) {
 function getEloScores(fromyear, toyear) {
     var url = `http://localhost:3001/basketball/getgames/${fromyear}/${toyear}`
 
-    $.ajax({url: url, dataType: "json"}).then(function (data) {
-        scores = {}
-        for (let i = 0; i < data.length; i++) {
-            let t1 = data[i].home_abbr, t2 = data[i].away_abbr
-            let s1 = data[i].home_score, s2 = data[i].away_score
+    scores = {}
+    fetch(url)
+        .then(res => res.json())
+        .then(
+            function (data) {
+                scores = {}
+                for (let i = 0; i < data.length; i++) {
+                    let t1 = data[i].home_abbr, t2 = data[i].away_abbr
+                    let s1 = data[i].home_score, s2 = data[i].away_score
 
-            let r1 = 0, r2 = 1;
-            if (s1 === s2) {
-                r1 = 0.5
-                r2 = 0.5
-            } else if (s1 > s2) {
-                r1 = 1
-                r2 = 0
+                    let r1 = 0, r2 = 1;
+                    if (s1 === s2) {
+                        r1 = 0.5
+                        r2 = 0.5
+                    } else if (s1 > s2) {
+                        r1 = 1
+                        r2 = 0
+                    }
+
+                    let e1 = get(scores, t1, 1000)
+                    let e2 = get(scores, t2, 1000)
+
+                    scores[t1] = Elo.getNewRating(e1, e2, r1)
+                    scores[t2] = Elo.getNewRating(e2, e1, r2)
+
+                }
             }
-
-            let e1 = get(scores, t1, 1000)
-            let e2 = get(scores, t2, 1000)
-
-            scores[t1] = Elo.getNewRating(e1, e2, r1)
-            scores[t2] = Elo.getNewRating(e2, e1, r2)
-
-        }
-    })
+        )
 }
+
+let scores = {}
+getEloScores(2019, 2019)
+console.log(scores)
