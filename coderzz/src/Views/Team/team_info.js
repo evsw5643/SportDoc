eeed416b1e68bbc5584dc7a4b5e3b1a69998e1df
Enrico Blackwell <pumpkintitan11@gmail.com>
2020-11-18 11:04:05 -0700

    maybe team elo

--- coderzz/src/Views/Team/team_info.js
+++ coderzz/src/Views/Team/team_info.js
@@ -4,6 +4,7 @@ import Blank from '../blank.png'
 // import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 function Team(props) {
 
+  const [elo, setelo] = useState({})
   const [loading, setloading] = useState(true);
   const [team, setteam] = useState([])
   const [sport, setsport] = useState("")
@@ -12,6 +13,7 @@ function Team(props) {
     setloading(true)
     setsport(props.sport)
     api(props.team, props.sport)
+    getEloScores(2019, 2019)
   }, [props.sport, props.team])
 
   function api(apiTeam, apiSport) {
@@ -27,6 +29,68 @@ function Team(props) {
           setloading(false)
         }
       )
+  }
+
+  const Elo = (function () {
+    function getRatingDelta(myRating, opponentRating, myGameResult) {
+      if ([0, 0.5, 1].indexOf(myGameResult) === -1) {
+        return null;
+      }
+
+      var myChanceToWin = 1 / (1 + Math.pow(10, (opponentRating - myRating) / 400));
+
+      return Math.round(100 * (myGameResult - myChanceToWin));
+    }
+
+    function getNewRating(myRating, opponentRating, myGameResult) {
+      return myRating + getRatingDelta(myRating, opponentRating, myGameResult);
+    }
+
+    function getWinChance(myRating, opponentRating) {
+      return 1 / (1 + Math.pow(10, (opponentRating - myRating) / 400))
+    }
+
+    return {
+      getRatingDelta: getRatingDelta,
+      getNewRating: getNewRating,
+      getWinChance: getWinChance // Elo.getWinChance
+    };
+  })();
+
+  function get(object, key, default_value) {
+    var result = object[key];
+    return (typeof result !== "undefined") ? result : default_value;
+  }
+
+  function getEloScores(fromyear, toyear) {
+    var url = `/basketball/getgames/${fromyear}/${toyear}`
+
+    let scores = {}
+    fetch(url)
+      .then(res => res.json())
+      .then(function (data) {
+        for (let i = 0; i < data.length; i++) {
+          let t1 = data[i].home_abbr, t2 = data[i].away_abbr
+          let s1 = data[i].home_score, s2 = data[i].away_score
+
+          let r1 = 0, r2 = 1;
+          if (s1 === s2) {
+            r1 = 0.5
+            r2 = 0.5
+          } else if (s1 > s2) {
+            r1 = 1
+            r2 = 0
+          }
+
+          let e1 = get(scores, t1, 1000)
+          let e2 = get(scores, t2, 1000)
+
+          scores[t1] = Elo.getNewRating(e1, e2, r1)
+          scores[t2] = Elo.getNewRating(e2, e1, r2)
+
+        }
+      })
+    setelo(scores)
   }
 
   function linkGen(type, sport, id) {
@@ -77,6 +141,8 @@ function Team(props) {
               src={linkGen("team", sport, team[0].abbreviation)}
               alt="Team logo" />
             <div className="card-text team_stat_text">
+              <div> ELO: {get(elo, 'DEN', 1000)} </div>
+              <br />
               <div> Career Points: {team[team.length - 1].points} </div>
               <br />
               <div> Career Assists: {team[team.length - 1].assists} </div>
