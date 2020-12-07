d77a60db3f570244de7a53d376c5acfb4b31af83
Enrico Blackwell <pumpkintitan11@gmail.com>
2020-12-07 13:45:12 -0700

    Sidebar and title

--- coderzz/src/Components/Sidebar/Sidebar.js
+++ coderzz/src/Components/Sidebar/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Sidebar.css'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData'
 import Search from '../../Views/Search/Search'
 
@@ -8,13 +9,16 @@ function Sidebar() {
         <div className="Sidebar">
             <ul className="SidebarList">
                 <li className="SidebarSearch">
-                    <Search/>
+                    <Search />
                 </li>
                 {SidebarData.map((val, key) => {
                     return (
-                        <li key={key} className="row" onClick={ () => {window.location.pathname = val.link} }>
-                            <div id="icon"> {val.icon} </div>
-                            <div id="title"> {val.title} </div>
+                        //onClick={ () => {window.location.pathname = val.link} }
+                        <li key={key} className="row" >
+                            <Link to={`${val.link}`}>
+                                <div id="icon"> {val.icon} </div>
+                                <div id="title"> {val.title} </div>
+                            </Link>
                         </li>
                     )
                 })}
